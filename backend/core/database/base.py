d780e9a05e194553a9582cd342f634ade2b75783
inaldev <166642110+DeveloperInal@users.noreply.github.com>
2025-02-20 23:52:27 +0300

    Update base.py

--- backend/core/database/base.py
+++ backend/core/database/base.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import uuid
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
@@ -13,20 +12,4 @@ def generate_cuid():
     return str(uuid.uuid4())
 
 class Base(DeclarativeBase, AsyncAttrs):
-=======
-import uuid
-
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from sqlalchemy import String
-from core.config import settings
-
-engine = create_async_engine(url=settings.url_database, echo=False)
-async_session = async_sessionmaker(bind=engine, expire_on_commit=True)
-
-def generate_cuid():
-    return str(uuid.uuid4())
-
-class Base(DeclarativeBase, AsyncAttrs):
->>>>>>> d8a3317a0a70d13af2213931a89ea36727542756
     id: Mapped[str] = mapped_column(String, primary_key=True, default=generate_cuid)
