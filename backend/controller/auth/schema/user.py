c5226305e4d91fa7722c606aedda488701bf2784
inaldev <166642110+DeveloperInal@users.noreply.github.com>
2025-02-20 23:54:21 +0300

    Update user.py

--- backend/controller/auth/schema/user.py
+++ backend/controller/auth/schema/user.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import strawberry
 from pydantic import BaseModel, EmailStr, ValidationError
 
@@ -36,43 +35,4 @@ def validate_user_data(username: str, email: str, password: str) -> UserModel:
             password=password)
         return user_model
     except ValidationError as e:
-=======
-import strawberry
-from pydantic import BaseModel, EmailStr, ValidationError
-
-class UserModel(BaseModel):
-    username: str
-    email: EmailStr
-    password: str
-
-@strawberry.type
-class User:
-    username: str
-    email: str
-    password: str
-
-@strawberry.type
-class LogoutResponse:
-    message: str
-
-@strawberry.type
-class Token:
-    access_token: str
-    refresh_token: str
-
-@strawberry.type
-class MessageResponse:
-    message: str
-    user_id: str
-    token: Token
-
-def validate_user_data(username: str, email: str, password: str) -> UserModel:
-    try:
-        user_model = UserModel(
-            username=username,
-            email=email, # type: ignore
-            password=password)
-        return user_model
-    except ValidationError as e:
->>>>>>> d8a3317a0a70d13af2213931a89ea36727542756
         raise ValueError(f"Ошибка валидации: {e}")
